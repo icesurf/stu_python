a490f3b6eac3bfba5ebba2d0684901e6979a5eed
LW_lin <120934657@qq.com>
2017-08-02 17:09:47 +0800

    新增糗百

--- sina.py
+++ sina.py
@@ -1,8 +1,8 @@
 from bs4 import BeautifulSoup
 soup = BeautifulSoup(open("sina2.html"))
-for ele in soup.find_all("a"):
-    if len(ele.contents) > 0 :
-        print ele.contents[0].encode("utf-8")
-    if "href" in ele.attrs:
-        print ele["href"].encode("utf-8")
-
+a = soup.find_all("a")
+for item in a:
+    if len(item.contents) > 0 :
+        print item.contents[0].encode("utf-8")
+    if "href" in item.attrs:
+        print item["href"].encode("utf-8")
